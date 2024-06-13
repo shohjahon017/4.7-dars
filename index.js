b63f5899c06fa09a768cc935565b48992e9a3f4f
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-13 14:26:25 +0500

    .

---
+++ index.js
@@ -0,0 +1,27 @@
+//1-masala
+// function colors(arg1, arg2, arg3) {
+//   let res = [];
+//   res.push(arg1);
+//   res.push(arg2);
+//   res.push(arg3);
+//   return res;
+// }
+
+// console.log(colors("qizil", "sariq", "yashil"));
+
+// //2-masala
+// function numbers(arg1, arg2, arg3) {
+//   let res = [arg1, arg2, arg3];
+//   return res;
+// }
+
+// console.log(numbers(1, 2, 3));
+
+//3-masala
+function murojat(arg1, arg2, arg3, arg4) {
+  let res = [arg1, arg2, arg3, arg4];
+  //   let res = [5, 6, 8, 9];
+  return res;
+}
+
+console.log(murojat[1]);
